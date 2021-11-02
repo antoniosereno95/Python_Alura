73482c913ac39a8a5c974b41d9bbd20d91b21c7d
toni <antoniojsereno@gmail.com>
2021-11-02 08:05:50 -0300

    Update modelo.py

--- Alura_POO_Python/Player_de_videos_POO(parte 2)_alura/modelo.py
+++ Alura_POO_Python/Player_de_videos_POO(parte 2)_alura/modelo.py
@@ -113,6 +113,44 @@ class Playlist(Programa):
     def programas_na_playlist(self):
         return self.__programas_na_playslist
 
+    def __str__(self):
+        return f'Playlist de nome: {self.nome_da_playlist}; Tamanho: {self.tamanho}; Lista de nomes: {self.reprensentaçao_nome_dos_objetos_da_playlist()};'
+
+    ##inicio
+    '''Se eu quero que o objeto Paylist se comporte como um iteravel 
+    eu devo utilizar uma artimanha chamada 'duck tiping', que vem do ditado
+    "se anda como um pato, voa como um pato e faz quack. entao deve ser um pato",
+    ou seja, se eu quero que o meu objeto se comporte como um iteravel eu devo colocar
+    metodos que adicione tais comportamentos ao mesmo. É oq vamos fazer quando implementarmos
+    o metodo '__getitem__' no codigo, pois esse metodo é um metodo que esta por debaixo dos 
+    panos em qualquer objeto iteravel do Python, logo, nosso objeto Playlist tendo os metodos
+    de iteraveis e tbm agindo como um iteravel o Python vai la e pensa 'ora, esse objeto deve
+    ser um iteravel tbm ' iguala o ditado.'''
+    def __getitem__(self, item):
+        return self.programas_na_playlist[item]
+    '''Aqui colocamos o metodo especial, muitas vezes tambem chamado de magico(magic method)
+    ou ate mesmo 'DANDER method' 
+    para retornar a lista que queremos iteragir com, pois o objeto iteravel deve ser iteragivel
+    com algo, entao nos vamos la e dizemos com oq o metodo deve ser itegarivel quando solicitarmos
+    tal ação la no __main__, dai temos que , quanod solicitado, o objeto deve atraves desse metodo
+    tornar iteravel cada item dentro da lista privada '__programas_na_playlista' que iremos integarir 
+    atraves da '@property' do atributo.'''
+    ##fim
+
+    ##inico
+    '''Para fazer com que o meu objeto tenha uma iterabilidade com o metodo len(), 
+    nao sendo ele uma lista(pq fizemos um ducktiping utilizando um dander method para que minhas
+    playlists se tornassem iteraveis como uma lista), teremos que implementar um novo dander method
+    ou magic method chamado '__len__' que vai fazer com que minha playlist tenha iterabilidade com 
+    o meu metodo len().
+    '''
+    def __len__(self):
+        return len(self.__programas_na_playslist)
+    '''dessa forma quanod o len() for chamado fora do objeto, ele vai returnar o resultado 
+    do metodo len() sobre a lista playlista que é uma variavel interna e por isso o 
+    metodo len() funciona dentro do objeto, e agora ele tambem ira funcionar fora do objeto '''
+    ##fim
+
     #metodos da classe
     #depois da herança em list eu nao preçido mais de uma funçao pois eu posso usar o len() direto no objeto
     @property
@@ -122,19 +160,22 @@ class Playlist(Programa):
     def retorna_lista_de_objetos_da_playlist(self):
         return self.__programas_na_playslist
 
+    '''Esse metodo 'retorna_lista_representada_em_string()' se torna 
+    obsoleto depois da implementaçao do __getitem__ mas vou deixar aqui
+    disponivel para fins de estudo de caso.'''
     def retorna_lista_representada_em_string(self):
         print(f"\n>Lista do Objeto tipo Playlist de nome({self.nome_da_playlist}) sendo representada em String<")
         for p in self.programas_na_playlist:
             print(p)
         print(">Fim da representaçao<\n")
+    ##
+
     def reprensentaçao_nome_dos_objetos_da_playlist(self):
         lista =[]
         for p in self.programas_na_playlist:
             lista.append(str(p.nome))
         return lista
 
-    def __str__(self):
-        return f'Playlist de nome: {self.nome_da_playlist}; Tamanho: {self.tamanho}; Lista de nomes: {self.reprensentaçao_nome_dos_objetos_da_playlist()};'
 
 
 ############### MAIN ###########################################
@@ -181,9 +222,18 @@ if(__name__ == "__main__"):
     #vou criar minha playlist
     playlist_de_fim_de_semana = Playlist("fim de semana",filmes_e_series)
     print(playlist_de_fim_de_semana)
-    # for p in playlist_de_fim_de_semana.programas_na_playlist:
-    #     print(p)
-    playlist_de_fim_de_semana.retorna_lista_representada_em_string()
+
+    #possivel com a implementaçao do __getitem__
+    ##inicio
+    print(demolidor in playlist_de_fim_de_semana)
+
+    for p in playlist_de_fim_de_semana:
+        print(p)
+    print(f'tamanho da lista usando metodo interno do objeto tamanho = {playlist_de_fim_de_semana.tamanho}')
+    print(f'tamanho da lista usando metodo len() = {len(playlist_de_fim_de_semana)}')
+    ##fim
+
+    #playlist_de_fim_de_semana.retorna_lista_representada_em_string()
 
 
 
