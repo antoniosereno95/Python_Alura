18c47711380eb4d8bdf7d9bac37d6e400ad897d1
toni <antoniojsereno@gmail.com>
2021-11-11 01:37:52 -0300

    Update Projeto_paralelo_locadora_de_carros.py

--- Projeto Paralelo - locadora de veiculos/Projeto_paralelo_locadora_de_carros.py
+++ Projeto Paralelo - locadora de veiculos/Projeto_paralelo_locadora_de_carros.py
@@ -1,3 +1,12 @@
+#back-up do conteudo do arquivo "Locadora_carros"
+"""
+AMG;merecedes-benz;2020;2;14;0;2;320;False\n
+TT;audi;2019;2;17;20000;2;280;False\n
+Compass;toyota;2018;4;15;25000;7;0;False\n
+Sprinter;mercedes;2016;3;16;43000;11;0;True\n
+Uno;fiat;2017;4;12;37000;5;0;False\n
+Sandero;renault;2016;2;13;48000;5;0;False\n
+"""
 ####### Classes ###################
 class Carro:
     def __init__(self,nome,marca,ano,numero_portas ,consumo_litros,kilometragem,lotaçao):
@@ -255,15 +264,24 @@ def Menu_aluguel():
     print("2.Alugar Moto")
     print("3.Alugar Barco")
     print("4.Voltar")
-    #print("*" * 42)
+    print("*" * 42)
 
 def Menu_Principal():
     print("*" * 42)
     print("     -Menu de Principal-   ")
     print("1.Alugar.")
     print("2.Devolver.")
-    print("3.Analizar Banco de Dados.")
+    print("3.Menu do Banco de Dados.")
     print("4.Sair")
+    print("*" * 42)
+
+def Menu_Banco_de_dados():
+    print("*" * 42)
+    print("     -Menu do Banco de Dados-   ")
+    print("1.Listagens.")
+    print("2.Adicionar.")
+    print("3.Reescrever.")
+    print("4.Sair.")
     print("*" * 42)
 
 def Cria_listas_de_objetos_CARROS():
@@ -277,13 +295,13 @@ def Cria_listas_de_objetos_CARROS():
                 linhas = linha_do_arquivo.split("\n")
                 '''linhas é uma lista e retorna esse conteudo aqui:
                 --> ['AMG;merecedes-benz;2020;2;14;0;2;320;False\\n', '']
-                #print(linhas)
-                '''
+                #print(linhas)'''
                 palavras = linhas[0].split(";")
                 palavras[8] = palavras[8].removesuffix("\\n")#tratamento da ultima string que vem com esse bagulho feioso ai no final dela.
                 #print(palavras)
 
                 if(int(palavras[6])>5 or palavras[8]=="True"):
+                    #tem que jogar um try aqui, pq se o tratamento de dados nao estiver correto o objeto nao vai conseguir ser gerado e a api vai crashar
                     carro_familiar = Carro_Familiar(str(palavras[0]),str(palavras[1]),int(palavras[2]),int(palavras[3]),float(palavras[4]),int(palavras[5]),int(palavras[6]),bool(palavras[8]))
                     Lista_de_obj_carros_familiares.append(carro_familiar)
                 if(int(palavras[7])!=0):
@@ -315,6 +333,151 @@ def Listagem_dos_Carros_Dinponiveis():
         print(f"{k}:")
         print(c)
         k += 1
+
+def Cria_listas_de_objetos_FUNCIONARIO():
+    Lista_de_obj_funcionarios = []
+    if(Existe_arquivo("Locadora_Funcionarios.txt")):
+        with open("Locadora_Funcionarios.txt","r") as arquivo:
+            for line in arquivo:
+                linha = line.split("\n")
+                #print(linha)
+                #linha = ['maria;F;30;12345678;madalena\\n']
+                palavras = linha[0].split(";")
+                palavras[4] = palavras[4].removesuffix("\\n")
+                try:
+                    funcionario = Funcionario(str(palavras[0]),str(palavras[1]),int(palavras[2]),str(palavras[3]),str(palavras[4]))
+                except: print(">>>tentativa de criar um objeto do tipo FUNCIONARIO fracassada!")
+                else:
+                    Lista_de_obj_funcionarios.append(funcionario)
+    return Lista_de_obj_funcionarios
+
+def Cria_listas_de_objetos_CLIENTES():
+    Lista_de_obj_clientes = []
+
+    if(Existe_arquivo("Locadora_Clientes.txt")):
+        with open("Locadora_Clientes.txt","r") as arquivo:
+            for line in arquivo:
+                linha = line.split("\n")
+                palavras = linha[0].split(";")
+                palavras[5] = palavras[5].removesuffix("\\n")
+                try:
+                    cliente = Cliente(str(palavras[0]),str(palavras[1]),int(palavras[2]),str(palavras[3]),str(palavras[4]),str(palavras[5]))
+                except:
+                    print(">>>tentativa de criar um objeto do tipo FUNCIONARIO mal sucedida!")
+                else:
+                    Lista_de_obj_clientes.append(cliente)
+    return Lista_de_obj_clientes
+
+def Valida_cpf_cliente(cpf):
+    for objeto in Lista_de_obj_clientes:
+        if(objeto.cpf == cpf):
+            return True
+        else:
+            return False
+
+def Login_banco_de_dados(nome,cpf):
+    for objeto in Lista_de_obj_funcionarios:
+        if(objeto.nome == nome and objeto.cpf == cpf):
+            return True
+        else:
+            return False
+
+def Adicioana_no_Arquivo_de_Carros():
+    #nome,marca,ano,numero_portas,consumo_litros,kilometragem,lotaçao,velocidade_maxima,van
+    print("~*~"*21)
+    print(">cadastro de carro<")
+    while(True):
+        print("Resposda ao questionario a seguir:")
+        nome = input("digite o nome do carro: ")
+        nome.strip()
+        marca = input("digite a marca do carro: ")
+        marca.strip()
+        ano = input("digite o ano do carro: ")
+        ano.strip()
+        numero_portas = input("digite o numero de portas do carro: ")
+        numero_portas.strip()
+        consumo_litros = input("digite o consumo de conbustivel medio do carro em litros: ")
+        consumo_litros.strip()
+        kilometragem = input("digite a kilometragem do carro: ")
+        kilometragem.strip()
+        lotacao = input("digite a lotação maxima do carro: ")
+        lotacao.strip()
+        print("O carro em questao se encaixa na categoria de carro esportivo?(s/n)")
+        r = input()
+        while r.upper() not in "SN":
+            r = input("(S/n):")
+        if(r.upper() == "S"):
+            velocidade_max = input("digite a velocidade maxima do carro em km/h: ")
+            velocidade_max.strip()
+        elif(r.upper() == "N"):
+            velocidade_max = 0
+        print("o carro em questao se encaixa na categoria van?(s/n)")
+        r = input()
+        while r.upper() not in "SN":
+            r = input("(S/n):")
+        if (r.upper() == "S"):
+            van = True
+        elif (r.upper() == "N"):
+            van = False
+
+        flag_erro = False
+        #carro familiar
+        if(int(lotacao) > 5 or van == True):
+            try:
+                c = Carro_Familiar(nome,marca,ano,numero_portas,consumo_litros,kilometragem,lotacao,van)
+            except:
+                print("erro ao tentar criar o objeto carro.")
+                flag_erro = True
+        #carro esportivo
+        if(velocidade_max != 0):
+            try:
+                c = Carro_Esportivo(nome,marca,ano,numero_portas,consumo_litros,kilometragem,lotacao,velocidade_max)
+            except:
+                print("erro ao tentar criar o objeto carro.")
+                flag_erro = True
+        #carro popular
+        if (int(lotacao) == 5):
+            try:
+                c = Carro_Popular(nome, marca, ano, numero_portas, consumo_litros, kilometragem, lotacao)
+            except:
+                print("erro ao tentar criar o objeto carro.")
+                flag_erro = True
+
+        #se der erro no objeto
+        if(flag_erro == True):
+            t = input("deseja tentar novamente o cadastro?(s/n)")
+            while t.upper() not in "SN":
+                t = input("(s/n)")
+            if(t.upper() == "S"):
+                pass
+            else:
+                break
+
+        #se der tudo certo
+        elif(flag_erro == False):
+            print("infos:")
+            print(c)
+            t = input("\ndeseja realmente adicionar essas informaçoes ao arquivo?(s/n)")
+            while t.upper() not in "SN":
+                t = input("(s/n)")
+            if (t.upper() == "S"):
+                Tratamento_de_infos_para_por_no_arquivo()
+            else:
+                t = input("deseja tentar novamente o cadastro?(s/n)")
+                while t.upper() not in "SN":
+                    t = input("(s/n)")
+                if (t.upper() == "S"):
+                    pass
+                else:
+                    break
+    print("~*~" * 21)
+    #fim
+
+def Adicioana_no_Arquivo_de_Clientes():
+    pass
+
+def Adicioana_no_Arquivo_de_Funcionarios():
+    pass
 
 ########## Metodos obsoletos ##############
 def Valida_cpf_cliente_por_arquivo(cpf_cliente):
@@ -358,10 +521,12 @@ if(__name__ == '__main__'):
 
     #antes de qualquer coisa acontecer eu vou criar as listas de objetos dos veiculos e Pessoas.(fica altamente mais facil e trabalhar assim)
     Lista_de_obj_carros_populares,Lista_de_obj_carros_familiares,Lista_de_obj_carros_esportivos = Cria_listas_de_objetos_CARROS()
-
+    Lista_de_obj_funcionarios = Cria_listas_de_objetos_FUNCIONARIO()
+    Lista_de_obj_clientes = Cria_listas_de_objetos_CLIENTES()
     #
 
     #inicio da rotina principal
+    #adicionar uma peuqena rotina de login do funcionario.
     while(True):
         Menu_Principal()
         r = input("Digite o numero da opção desejada: ")
@@ -374,25 +539,21 @@ if(__name__ == '__main__'):
                 while q.upper() not in "SN":
                     q = input("digite uma opçao valida(s/n): ")
                 if(q.upper() == "S"):
-                    flag_cpf = False
-                    C_cpf = input("Digite o CPF do cliente: ")
-                    if(C_cpf != 0):
-                        # validar cpf dentro da lista de objetos de Clientes
-                        # if(Valida_cpf_cliente()):
-                        #     flag_cpf= True
-                        # else:
-                        #     flag_cpf = False
+                    C_cpf = str(input("Digite o CPF do cliente: "))
+                    if(Valida_cpf_cliente(C_cpf)):
                         flag_cpf = True
+                        print(">Cliente encontrado em nossos arquivos<")
+                    else:
+                        flag_cpf = False
+
                 elif(q.upper() == "N"):
                     t = input("Deseja cadastrar o cliente agora?(S/N) ")
                     while t.upper() not in "SN":
                         t = input("digite uma opçao valida(s/n): ")
                     if(q.upper() == "S"):
-                        print("Sessao ainda nao desenvolvida.")
-                    #     Cadastrar_Cliete()
-                    #     pass
-                    # else:
-                    #     continue
+                        Adicioana_no_Arquivo_de_Clientes()
+                    else:
+                        continue
 
                 if(flag_cpf == True):
                         Menu_aluguel()
@@ -421,15 +582,77 @@ if(__name__ == '__main__'):
                                 print("*" * 42)
                                 continue
 
-
         if(r == "2"):#Devolver
             Menu_devoluçao()
             #na devoluçao o atendente primeiro diz oq vais ser devolvido
-            # depois ele da o cpf ou placa do veiculo a ser devolvido
+            # depois ele da o cpf ou placa do veiculo(que nao tem variavel implementada ainda) a ser devolvido
 
-        if(r == "3"):
-            #analize dos arquivos
-            pass
+        if(r == "3"):#opçoes do banco de dados
+            print(">ACESSO RESTRITO, LOGIN NECESSARIO<")
+            F_nome = str(input("Digite seu nome:"))
+            F_cpf = str(input("Digite seu cpf:"))
+            if(Login_banco_de_dados(F_nome,F_cpf) == False):
+                print("*" * 42)
+                print("Acesso negado, area designidade somente para funcionarios cadastrados.")
+                print("*" * 42)
+            else:
+                print("Acesso consedido...")
+                while(True):
+                    Menu_Banco_de_dados()
+                    opçao = input("Digite a opção desejada: ")
+                    while opçao not in "1234":
+                        opçao = input("Digite uma opção valida(1,2,3,4): ")
+
+                    if(opçao == "1"):#listagem
+                        print("Desaja que seja listada que tipo de arquivo?")
+                        print("1.carros\n2.clientes\n3.funcionarios")#depois implementar o resto das opçoes
+                        op = input("Digite sua escolha: ")
+                        while op not in "123":
+                            op = input("Digite sua escolha(1,2 ou 3): ")
+                        if(op == "1"):
+                            #depois arrumar isso aqui, pq um carro pode nao estar disponivel mas ele tem que aparecer na listagem do banco de dados.
+                            print("-"*42)
+                            Listagem_dos_Carros_Dinponiveis()
+                            print("-" * 42)
+                        elif(op == "2"):
+                            print("-" * 42)
+                            for ob in Lista_de_obj_clientes:
+                                print(ob)
+                            print("-" * 42)
+                        elif(op == "3"):
+                            print("-" * 42)
+                            for ob in Lista_de_obj_funcionarios:
+                                print(ob)
+                            print("-" * 42)
+
+                    elif(opçao == "2"):#adicionar
+                        print("onde desaja adicionar dados?")
+                        print("1.carros\n2.clientes\n3.funcionarios")# depois implementar o resto das opçoes
+                        op = input("Digite sua escolha: ")
+                        while op not in "123":
+                            op = input("Digite sua escolha(1,2 ou 3): ")
+                        if (op == "1"):
+                            Adicioana_no_Arquivo_de_Carros()
+                        elif (op == "2"):
+                            Adicioana_no_Arquivo_de_Clientes()
+                        elif (op == "3"):
+                            Adicioana_no_Arquivo_de_Funcionarios()
+
+                    elif(opçao == "3"):#modificar
+                        #1 objeto modificado por vez
+                        #ficar de olho pois se eu modificar a velo maxima ou o atributo van, ai terei de tirar o objeto de uma lista e arquivo e colocar no outro arquivo e na outra lista !!!!
+                        pass
+                    elif(opçao == "4"):
+                        q = input("Deseja realmente sair do 'Menu do Banco de Dados'?(s/n)")
+                        while q.upper() not in "SN":
+                            q = input("digite uma opçao valida(s/n): ")
+                        if (q.upper() == "S"):
+                            print("Voltando ao menu principal...")
+                            break
+                        else:
+                            print("*" * 42)
+                            continue
+
 
         if(r == "4"):#Sair
             q = input("Deseja realmente encerrar o programa?(s/n)")
@@ -440,7 +663,4 @@ if(__name__ == '__main__'):
                 print("Encerrando o programa...")
                 break
             else: print("Voltando ao menu principal...")
-
-
-
-
+        #fim da rotina principal
